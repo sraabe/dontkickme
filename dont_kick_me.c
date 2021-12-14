25da91a0e2981f1fe06163a1eab6b7562c98f39a
Steffen Raabe <steffen@raabe.io>
2021-12-14 14:39:56 +0100

    Added timestamp and pid output.

--- dont_kick_me.c
+++ dont_kick_me.c
@@ -1,13 +1,19 @@
 #include <windows.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+
+#ifndef NULL
+#define NULL ((void *) 0)
+#endif
 
 HWND hWnd;
-time_t t;
-int i;
-int sleep_ms;
+time_t t, ts;
+char *ts_s;
+int i, sleep_ms;
+long unsigned int *pid;
 
-LPCSTR ffxiv_window_name = "FINAL FANTASY XIV";
+const char *ffxiv_window_name = "FINAL FANTASY XIV";
 char keys[5] = {
     0x57,
     0x53,
@@ -19,6 +25,9 @@ char keys[5] = {
 int main(int argc, char **argv)
 {
     hWnd = FindWindow(NULL, ffxiv_window_name);
+    pid = malloc(sizeof(long unsigned int));
+
+    GetWindowThreadProcessId(hWnd, pid);
     srand((unsigned) time(&t));
 
     do
@@ -28,7 +37,10 @@ int main(int argc, char **argv)
         Sleep(rand() % 50 + 150);
         SendMessage(hWnd, WM_KEYUP, keys[i], 0x00010003);
         sleep_ms = (rand() % 600 + 600) * 1000;
-        fprintf(stdout, "Sent keypress \"%c\", now sleeping for %d seconds..\n", keys[i], sleep_ms / 1000);
+        ts = time(NULL);
+        ts_s = ctime(&ts);
+        ts_s[strlen(ts_s) - 1] = 0;
+        fprintf(stdout, "[%s] [pid:%lu] Sent keypress \"%c\", now sleeping for %d seconds..\n", ts_s, *pid, keys[i], sleep_ms / 1000);
         Sleep(sleep_ms);
     } while (1);
 
